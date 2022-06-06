b35ce47c6c268243a5f87e72edf1f161671d896c
Dia Leor Paule <dia.paule@gmail.com>
2022-06-06 18:47:55 +0800

    Create outline for Section 1.3 notes on stratification and variance

---
+++ intro_to_regression/intro-3.R
@@ -0,0 +1,14 @@
+## Section 1.3 - Introduction to Regression: Stratification and Variance Explained
+
+# Anscombe's Quartet/Stratification ---------------------------------------
+
+
+# Bivariate Normal Distribution -------------------------------------------
+
+
+# Variance Explained ------------------------------------------------------
+
+
+# There are Two Regression Lines ------------------------------------------
+
+
