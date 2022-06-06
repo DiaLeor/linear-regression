c6ff9b225d7de74d1cd04ce5cfa494b23e5c1994
Dia Leor Paule <dia.paule@gmail.com>
2022-06-06 18:16:45 +0800

    Update Section 1.2 notes on correlation

--- intro_to_regression/intro-2.R
+++ intro_to_regression/intro-2.R
@@ -38,8 +38,9 @@ galton_heights %>%
 # standardized values: ((x_i - mu_x)/sigma_x)*((y_i - mu_i)/sigma_y) -- with mu being the symbol representing the
 # mean and sigma representing SD.
 
-# The Greek letter rho is commonly used in the statistics book to denote this correlationm, as rho is the Greek
+# The Greek letter rho is commonly used in the statistics book to denote this correlation, as rho is the Greek
 # letter for 'r,' the first letter of regression.
+rho <- mean(scale(x)*scale(y))
 
 # The correlation coefficient essentially conveys how two variables move together.
 
@@ -50,10 +51,14 @@ galton_heights %>%
 # The correlation is this average, which will be about 0. Therefore, unrelated variables will have a correlation of
 # about 0.
 
+# If the quantities vary together, we are averaging mostly positive products and see a positive correlation
+# (positive*positive or negative*negative).
+# If they vary in opposite directions, we get a negative correlation (positive*negative and vice versa).
+
 # The correlation coefficient is always between -1 and 1.
 
 # ..Code..
-rho <- mean(scale(x)*scale(y))
+# the correlation between father and sons' height
 galton_heights %>% summarize(r = cor(father, son)) %>% pull(r)
 
 # Sample Correlation is a Random Variable ---------------------------------
@@ -61,15 +66,18 @@ galton_heights %>% summarize(r = cor(father, son)) %>% pull(r)
 # The correlation that we compute and use as a summary is a random variable.
 
 # When interpreting correlations, it is important to remember that correlations derived from samples are estimates
-# containing uncertainty.
+# containing uncertainty. It's a random variable, and it can have a pretty large standard error.
 
-# Because the sample correlation is an average of independent draws, the central limit theorem applies.
+# Because the sample correlation is an average of independent draws, the central limit theorem applies. Therefore,
+# for a large enough sample size N, the distribution of these Rs is approximately normal.
+
+# The formula to derive the standard deviation R ~ N (rho, sqrt((1-r^2)/(N-2))) 
 
 # ..Code..
-# compute sample correlation
+# compute sample correlation, sample size 25
 R <- sample_n(galton_heights, 25, replace = TRUE) %>%
   summarize(r = cor(father, son))
-R
+R # R is a random variable
 
 # Monte Carlo simulation to show distribution of sample correlation
 B <- 1000
@@ -82,8 +90,8 @@ R <- replicate(B, {
 qplot(R, geom = "histogram", binwidth = 0.05, color = I("black"))
 
 # expected value and standard error
-mean(R)
-sd(R)
+mean(R) # We see that the expected value is the population correlation, the mean of these Rs
+sd(R) # and that it has a relatively high standard error relative to its size.
 
 # QQ-plot to evaluate whether N is large enough
 data.frame(R) %>%
