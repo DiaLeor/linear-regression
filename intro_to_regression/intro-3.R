88a1a9c889fcd4ab70449e4811afaf9b18226322
Dia Leor Paule <dia.paule@gmail.com>
2022-06-08 17:27:23 +0800

    Update Section 1.3 notes on stratification and variance

--- intro_to_regression/intro-3.R
+++ intro_to_regression/intro-3.R
@@ -160,4 +160,22 @@ galton_heights %>%
 
 # There are Two Regression Lines ------------------------------------------
 
+# There are two different regression lines depending on whether we are taking the expectation of Y given X or
+# taking the expectation of X given Y.
 
+# x = {E(Y | X = x) - b}/0.5
+# y = E(X | Y = y) = 34 + 0.5*y
+
+# ..Code..
+# compute a regression line to predict the son's height from the father's height
+mu_x <- mean(galton_heights$father)
+mu_y <- mean(galton_heights$son)
+s_x <- sd(galton_heights$father)
+s_y <- sd(galton_heights$son)
+r <- cor(galton_heights$father, galton_heights$son)
+m_1 <-  r * s_y / s_x
+b_1 <- mu_y - m_1*mu_x
+
+# compute a regression line to predict the father's height from the son's height
+m_2 <-  r * s_x / s_y # new slope
+b_2 <- mu_x - m_2*mu_y # new intercept
